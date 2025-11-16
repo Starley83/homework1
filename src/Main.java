57e104cb6d18f28b60725c3b5a3bf88407d114e1
Сергей <sergeym83@bk.ru>
2025-11-16 21:51:59 +0300

    homework3 изменены типы переменных

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,16 @@ public class Main {
 
         int a = 52357; //задача 1
         byte b = 15;
-        short c = 150;
+        short C = 150;
         long d = 3150456789L;
         float e = 5.67584f;
         double f = 9.19056789;
+        System.out.println("значение переменной a c типом int равно " + a);
+        System.out.println("значение переменной b c типом byte равно " + b);
+        System.out.println("значение переменной C c типом short равно " + C);
+        System.out.println("значение переменной d c типом long равно " + d);
+        System.out.println("значение переменной e c типом float равно " + e);
+        System.out.println("значение переменной f c типом double равно " + f);
 
         float g = 27.12f;  //задача 2
         long h = 987678965549L;
@@ -18,15 +24,15 @@ public class Main {
         short l = 27897;
         byte m = 67;
 
-        int LP = 23;  //задача 3
-        int AS = 27;
-        int EA = 30;
-        int list = 480;
+        byte LP = 23;  //задача 3
+        byte AS = 27;
+        byte EA = 30;
+        short list = 480;
         int anyone = list / (LP + AS + EA);
         System.out.println("на каждого ученика расссчитано " + anyone + " листов бумаги");
 
-        int bottles = 16;  //задача 4
-        int time = 2;
+        byte bottles = 16;  //задача 4
+        byte time = 2;
         int bottlesPerMinute = bottles / time;
         int period = 20;
         int bottlesPerPeriod = bottlesPerMinute * period;
@@ -41,27 +47,27 @@ public class Main {
         bottlesPerPeriod = bottlesPerMinute * period;
         System.out.println("За месяц (30 дней) машина произвела " + bottlesPerPeriod + " штук бытулок");
 
-        int paint = 120; //задача 5
-        int white = 2;
-        int brown = 4;
+        byte paint = 120; //задача 5
+        byte white = 2;
+        byte brown = 4;
         int paintForClass = white + brown;
         int numberOfClasses = paint / paintForClass;
         int numberOfWhite = white * numberOfClasses;
         int numberOfBrown = brown * numberOfClasses;
         System.out.println("В школе, где " + numberOfClasses + " классов, нужно " + numberOfWhite + " банок белой краски и " + numberOfBrown + " банок коричневой краски.");
 
-        int weightOfBanan = 80; // задача 6
-        int weightOfMilk = 105;
-        int weightOfPlombir = 100;
-        int weightOfEgg = 70;
+        byte weightOfBanan = 80; // задача 6
+        byte weightOfMilk = 105;
+        byte weightOfPlombir = 100;
+        byte weightOfEgg = 70;
         int weightOfCoctail = weightOfBanan * 5 + weightOfMilk * 2 + weightOfPlombir * 2 + weightOfEgg * 4;
         float KG = (float) weightOfCoctail / 1000;
         System.out.println("Вес коктейля " + weightOfCoctail + " грамм");
         System.out.println("Вес коктейля " + KG + " килограмм");
 
-        int goal = 7000; //задача 7
-        int minWeight = 250;
-        int maxWeight = 500;
+        short goal = 7000; //задача 7
+        short minWeight = 250;
+        short maxWeight = 500;
         int minGoal = goal / minWeight;
         int maxGoal = goal / maxWeight;
         int midGoal = (minGoal + maxGoal) / 2;
@@ -81,7 +87,5 @@ public class Main {
         System.out.println("Маша теперь получает " + newPayMasha + " рублей. Годовой доход вырос на " + deltaMasha + " рублей");
         System.out.println("Денис теперь получает " + newPayDenis + " рублей. Годовой доход вырос на " + deltaDenis + " рублей");
         System.out.println("Кристина теперь получает " + newPayKristina + " рублей. Годовой доход вырос на " + deltaKristina + " рублей");
-
-
     }
 }
