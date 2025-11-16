0f2635009d407593c79bf00a4ed4258fc330d425
Сергей <sergeym83@bk.ru>
2025-11-16 19:30:52 +0300

    homework 2 (переменные и синтаксис)

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         var dog = 8.0;  //задача 1
         var cat = 3.6;
         var paper = 763789;
