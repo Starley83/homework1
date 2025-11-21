0f12c370529c9117df15515484ed70fbd648c14f
Сергей <sergeym83@bk.ru>
2025-11-22 01:59:47 +0300

    homework5

--- src/Main.java
+++ src/Main.java
@@ -2,90 +2,69 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int clientOS = 0; //задача 1
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
 
-        int a = 52357; //задача 1
-        byte b = 15;
-        short C = 150;
-        long d = 3150456789L;
-        float e = 5.67584f;
-        double f = 9.19056789;
-        System.out.println("значение переменной a c типом int равно " + a);
-        System.out.println("значение переменной b c типом byte равно " + b);
-        System.out.println("значение переменной C c типом short равно " + C);
-        System.out.println("значение переменной d c типом long равно " + d);
-        System.out.println("значение переменной e c типом float равно " + e);
-        System.out.println("значение переменной f c типом double равно " + f);
+        int clientDeviceYear = 2016;  // задача 2
+        if (clientDeviceYear >= 2015 && clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientDeviceYear >= 2015 && clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
 
-        float g = 27.12f;  //задача 2
-        long h = 987678965549L;
-        float i = 2.786f;
-        short j = 569;
-        short k = -159;
-        short l = 27897;
-        byte m = 67;
+        int year = 2021;  //задача 3
+        if (year < 1584) {
+            System.out.println(year + " год не является високосным");
+        } else if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
 
-        byte LP = 23;  //задача 3
-        byte AS = 27;
-        byte EA = 30;
-        short list = 480;
-        int anyone = list / (LP + AS + EA);
-        System.out.println("на каждого ученика расссчитано " + anyone + " листов бумаги");
+        int deliveryDistance = 95; //задача 4
+        int deliveryTime = 1;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: " + deliveryTime);
+        } else if (deliveryDistance <= 60) {
+            System.out.println("Потребуется дней: " + (deliveryTime + 1));
+        } else if (deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: " + (deliveryTime + 2));
+        } else {
+            System.out.println("Доставки нет");
+        }
 
-        byte bottles = 16;  //задача 4
-        byte time = 2;
-        int bottlesPerMinute = bottles / time;
-        int period = 20;
-        int bottlesPerPeriod = bottlesPerMinute * period;
-        System.out.println("За 20 минут машина произвела " + bottlesPerPeriod + " штук бытулок");
-        period = 60 * 24;
-        bottlesPerPeriod = bottlesPerMinute * period;
-        System.out.println("За сутки машина произвела " + bottlesPerPeriod + " штук бытулок");
-        period = period * 3;
-        bottlesPerPeriod = bottlesPerMinute * period;
-        System.out.println("За трое суток машина произвела " + bottlesPerPeriod + " штук бытулок");
-        period = period * 10;
-        bottlesPerPeriod = bottlesPerMinute * period;
-        System.out.println("За месяц (30 дней) машина произвела " + bottlesPerPeriod + " штук бытулок");
-
-        byte paint = 120; //задача 5
-        byte white = 2;
-        byte brown = 4;
-        int paintForClass = white + brown;
-        int numberOfClasses = paint / paintForClass;
-        int numberOfWhite = white * numberOfClasses;
-        int numberOfBrown = brown * numberOfClasses;
-        System.out.println("В школе, где " + numberOfClasses + " классов, нужно " + numberOfWhite + " банок белой краски и " + numberOfBrown + " банок коричневой краски.");
-
-        byte weightOfBanan = 80; // задача 6
-        byte weightOfMilk = 105;
-        byte weightOfPlombir = 100;
-        byte weightOfEgg = 70;
-        int weightOfCoctail = weightOfBanan * 5 + weightOfMilk * 2 + weightOfPlombir * 2 + weightOfEgg * 4;
-        float KG = (float) weightOfCoctail / 1000;
-        System.out.println("Вес коктейля " + weightOfCoctail + " грамм");
-        System.out.println("Вес коктейля " + KG + " килограмм");
-
-        short goal = 7000; //задача 7
-        short minWeight = 250;
-        short maxWeight = 500;
-        int minGoal = goal / minWeight;
-        int maxGoal = goal / maxWeight;
-        int midGoal = (minGoal + maxGoal) / 2;
-        System.out.println("при похудении на 250 грамм, время похудения " + minGoal + " дней");
-        System.out.println("при похудении на 500 грамм, время похудения " + maxGoal + " дней");
-        System.out.println("В среднем, время похудения составило " + midGoal + " дней");
-
-        int payMasha = 67760;  //задача 8
-        int payDenis = 83690;
-        int payKristina = 76230;
-        int newPayMasha = payMasha + payMasha / 10;
-        int newPayDenis = payDenis + payDenis / 10;
-        int newPayKristina = payKristina + payKristina / 10;
-        int deltaMasha = payMasha * 12 / 10;
-        int deltaDenis = payDenis * 12 / 10;
-        int deltaKristina = payKristina * 12 / 10;
-        System.out.println("Маша теперь получает " + newPayMasha + " рублей. Годовой доход вырос на " + deltaMasha + " рублей");
-        System.out.println("Денис теперь получает " + newPayDenis + " рублей. Годовой доход вырос на " + deltaDenis + " рублей");
-        System.out.println("Кристина теперь получает " + newPayKristina + " рублей. Годовой доход вырос на " + deltaKristina + " рублей");
+        int monthNumber = 12; //задача 5
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Это зимний месяц");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Это весенний месяц");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Это летний месяц");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Это весенний месяц");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+        }
     }
 }
